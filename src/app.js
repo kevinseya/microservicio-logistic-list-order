3b75c6adc4aff723c3066923f1355c75e129c15f
lunakenya <luna28kenya@gmail.com>
2025-02-02 19:33:33 -0500

    feat: :rocket: Initialize Express server in app.js

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/dbConfig'); 
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { graphqlHTTP } = require('express-graphql');
+
+
+const app = express();
+
+// Middleware setup
+app.use(bodyParser.json());
+
+// Configuration de GraphQL
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true, // Activate the test interface GraphiQL
+}));
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+// Synchronize models with the database
+sequelize.sync({ alter: true })
+    .then(() => {
+        console.log('Database synchronized successfully.');
+    })
+    .catch((error) => {
+        console.error('Failed to synchronize database:', error);
+    });
